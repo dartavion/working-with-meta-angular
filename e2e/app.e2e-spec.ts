09c07278e73357597fc25ed7ea41ecf42a8ebc9f
dartavion <richardranke@gmail.com>
2017-06-25 13:40:11 -0400

    adding simple e2e form valid/submit

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,5 +1,8 @@
 import { WorkingWithMetaAngularPage } from './app.po';
-
+import { by } from 'protractor';
+function hasClass( target, className ) {
+  return new RegExp('(\\s|^)' + className + '(\\s|$)').test(target.className);
+}
 describe('working-with-meta-angular App', () => {
   let page: WorkingWithMetaAngularPage;
 
@@ -9,6 +12,27 @@ describe('working-with-meta-angular App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(page.getParagraphText()).toEqual('Find a Car');
+  });
+
+  it('should submit a valid form', () => {
+    const firstName = page.getFirstNameField(),
+      lastName = page.getLastNameNameField(),
+      year = page.getYearField(),
+      make = page.getMakeField(),
+      model = page.getModelField(),
+      submitButton = page.getSubmitButton(),
+      form = page.getForm();
+
+    expect(form.getAttribute('class')).toContain('ng-invalid');
+
+    firstName.sendKeys('test');
+    lastName.sendKeys('smith');
+    year.element(by.cssContainingText('option', '2017')).click();
+    make.element(by.cssContainingText('option', 'bmw')).click();
+    model.element(by.cssContainingText('option', '328xi')).click();
+    submitButton.click();
+
+    expect(form.getAttribute('class')).toContain('ng-valid');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -8,4 +8,32 @@ export class WorkingWithMetaAngularPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getFirstNameField() {
+    return element(by.name('/transforms/data/names/first'));
+  }
+
+  getLastNameNameField() {
+    return element(by.name('/transforms/data/names/last'));
+  }
+
+  getYearField() {
+    return element(by.name('/transforms/data/automobile/year'));
+  }
+
+  getMakeField() {
+    return element(by.name('/transforms/data/automobile/make'));
+  }
+
+  getModelField() {
+    return element(by.name('/transforms/data/automobile/model'));
+  }
+
+  getSubmitButton() {
+    return element(by.name('submitButton'));
+  }
+
+  getForm() {
+    return element(by.css('form'));
+  }
 }
