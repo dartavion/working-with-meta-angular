458bd74f9167d544a12dc01d67cc5801dc409569
dartavion <richardranke@gmail.com>
2017-06-25 16:30:21 -0400

    adding basic tests to meta-input

--- src/app/components/meta-input/meta-input.component.spec.ts
+++ src/app/components/meta-input/meta-input.component.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
+
 import { CUSTOM_ELEMENTS_SCHEMA, DebugElement, Injectable } from '@angular/core';
 
 import { MetaInputComponent } from './meta-input.component';
@@ -9,11 +9,14 @@ import { MetaHttpService } from '../../services/meta-http.service';
 import { HttpModule } from '@angular/http';
 import { MetaFormConverterService } from '../../services/meta-form-converter.service';
 import { MetaFormConfigService } from '../../services/meta-form-config.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of'
 
 describe('MetaInputComponent', () => {
   let component: MetaInputComponent;
   let fixture: ComponentFixture<MetaInputComponent>;
-
+  let mockMetaHttpService: MockMetaHttpService;
+  let mockMetaFormConverterService: MockMetaFormConverterService;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -23,8 +26,8 @@ describe('MetaInputComponent', () => {
       declarations: [ MetaInputComponent ],
       providers: [
         FormBuilder,
-        MetaHttpService,
-        MetaFormConverterService,
+        {provide: MetaHttpService, useClass: MockMetaHttpService},
+        {provide: MetaFormConverterService, useClass: MockMetaFormConverterService},
         MetaFormConfigService
       ],
       schemas: [
@@ -41,10 +44,53 @@ describe('MetaInputComponent', () => {
     component.metaPath = '/string/other';
     component.form = form.group({user: form.group({firstName: ''})});
     component.formGroupName = 'user';
+    mockMetaHttpService = TestBed.get(MetaHttpService);
+    mockMetaFormConverterService = TestBed.get(MetaFormConverterService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('subscribes to meta http service onInit', () => {
+    const testData = {
+      key: 234,
+      label: 'longName',
+      required: false,
+      disabled: false,
+      controlType: 'textInput'
+    };
+
+    spyOn(mockMetaHttpService, 'get').and.returnValue(Observable.of(testData));
+    spyOn(mockMetaFormConverterService, 'getMetaInput');
+    spyOn(component, 'assignFormGroup').and.returnValue(testData);
+
+    component.ngOnInit();
+
+    expect(mockMetaHttpService.get).toHaveBeenCalled();
+    expect(mockMetaFormConverterService.getMetaInput).toHaveBeenCalled();
+    expect(component.assignFormGroup).toHaveBeenCalled();
+  });
+
+  it('unsubscribes to the meta http service onDestroy', () => {
+    spyOn(component.metaHttpServiceSubscription, 'unsubscribe');
+    component.ngOnDestroy();
+    expect(component.metaHttpServiceSubscription.unsubscribe).toHaveBeenCalled();
+  });
+
+  xit('assigns meta field to a form group', () => {
+    // TODO
+  });
 });
+
+export class MockMetaHttpService {
+  get() {
+    return Observable.of([1, 1, 1, 1]).map(data => data);
+  }
+}
+export class MockMetaFormConverterService {
+  getMetaInput() {
+    return {}
+  }
+}
--- src/app/components/meta-input/meta-input.component.ts
+++ src/app/components/meta-input/meta-input.component.ts
@@ -1,14 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup, FormGroupName, Validators } from '@angular/forms';
 import { MetaFormConverterService } from '../../services/meta-form-converter.service';
 import { MetaHttpService } from '../../services/meta-http.service';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'meta-input',
   templateUrl: './meta-input.component.html',
   styleUrls: ['./meta-input.component.scss']
 })
-export class MetaInputComponent implements OnInit {
+export class MetaInputComponent implements OnInit, OnDestroy {
   @Input() metaPath: string;
   @Input() name: string;
   @Input() form: FormGroup;
@@ -18,22 +20,25 @@ export class MetaInputComponent implements OnInit {
   public required: boolean;
   public label: string;
   public options: Array<any>;
-  private meta: any;
+  public metaHttpServiceSubscription: Subscription;
 
   constructor(
     private metaHttpService: MetaHttpService,
     private metaFormConverter: MetaFormConverterService) { }
 
   ngOnInit() {
-    this.metaHttpService
+    this.metaHttpServiceSubscription = this.metaHttpService
       .get()
       .subscribe(meta => {
-        this.meta = meta;
-        this.getGroup(this.metaFormConverter.getMetaInput(this.metaPath, meta));
+        this.assignFormGroup(this.metaFormConverter.getMetaInput(this.metaPath, meta));
       });
   }
 
-  getGroup(metaInput: any) {
+  ngOnDestroy() {
+    this.metaHttpServiceSubscription.unsubscribe();
+  }
+
+  assignFormGroup(metaInput: any) {
     if (metaInput.hasOwnProperty('input')) {
       this.parseMeta(metaInput.input);
       this.applyFormProperties(metaInput.input);
@@ -46,7 +51,7 @@ export class MetaInputComponent implements OnInit {
     }
   }
 
-  applyFormProperties(metaInput: any) {
+  private applyFormProperties(metaInput: any) {
     const control = this.form.get(`${this.formGroupName}.${this.metaPath}`);
 
     this.parseMeta(metaInput);
@@ -61,10 +66,12 @@ export class MetaInputComponent implements OnInit {
     }
   }
 
-  parseMeta(metaInput: any) {
+  private parseMeta(metaInput: any) {
     this.label = metaInput.label;
     this.type = metaInput.controlType;
     this.required = metaInput.required;
-    this.options = metaInput.options || [];
+    if (metaInput.options) {
+      this.options = metaInput.options || [];
+    }
   }
 }
